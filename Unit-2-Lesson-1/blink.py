f85fd8107f9e40775eb22ecb0553ea63c1530527
Caleb Andreas <caleb.andreas@mths.ca>
2026-02-18 16:13:42 -0500

    Add files via upload
    made blink in python

---
+++ Unit-2-Lesson-1/blink.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Created by: Caleb Andreas
+Created on: February 2026
+This program is the blink program for the pi pico.
+"""
+
+import board
+import digitalio
+import time
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+    led.value = True
+    time.sleep(1)
+    led.value = False
+    time.sleep(1)
