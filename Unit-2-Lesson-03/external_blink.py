167372f6f304e71f0ebf1cb5c1ffd34af6b5c647
Caleb Andreas <caleb.andreas@mths.ca>
2026-02-24 15:48:05 -0500

    renamed folder and made external blink program in python

---
+++ Unit-2-Lesson-03/external_blink.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Created by: Caleb Andreas
+Created on: February 24, 2026
+This program is the blink program for the pi pico with an external LED.
+"""
+
+import board
+import digitalio
+import time
+
+# Setup.
+led = digitalio.DigitalInOut(board.GP5)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+    led.value = True
+    time.sleep(1)
+    led.value = False
+    time.sleep(1)
